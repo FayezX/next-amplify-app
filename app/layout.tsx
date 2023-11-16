d111337f7d97b91789b9cc86097cf544ac7519e0
FayezX <Fayahm3@gmail.com>
2023-11-15 21:40:53 -0500

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { Amplify } from "aws-amplify";
-import awsExports from "../aws-exports";
-
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from '../amplifyconfiguration.json';
 import "@aws-amplify/ui-react/styles.css";
 
-Amplify.configure({ ...awsExports, ssr: true });
+Amplify.configure({ ...amplifyconfig, ssr: true });
 
 const inter = Inter({ subsets: ['latin'] })
 
